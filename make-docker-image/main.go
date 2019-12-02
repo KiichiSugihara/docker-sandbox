538b8ce60fdaad581491a90bb57159486b88d26f
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-02 09:07:27 +0900

    add make-docker-image

---
+++ make-docker-image/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("received request")
+		fmt.Fprintln(w, "Hello Docker!!")
+	})
+}
